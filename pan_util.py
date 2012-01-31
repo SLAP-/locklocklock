70c80d97c1205dd145a2a1e46bfff27c2e434fb1
pxy <xiaoyue.pan@it.uu.se>
2012-01-31 17:25:28 +0100

    working copy

--- pan_util.py
+++ pan_util.py
@@ -7,6 +7,7 @@ from lockgraph import *
 if_print = 1
 
 class1_idx = 0
+class2_idx = 1
 class3_idx = 2
 
 ts_idx = 1
@@ -27,7 +28,15 @@ def get_contention_overhead(t1,t3):
 	a = 8.63685e+09
 	b = -1.27817e+10
 	c = 56357
-	return a*t1+b*t1+c
+	return a*t1*t1+b*t1+c
+def get_contention_overhead_asymmetric(t1,t3):
+	a = -2.88363e+15 
+	b = 8.11897e+15 
+	c = -4.18053e+15
+	d = 1.06434e+11 
+	e = -4.45469e+10
+	f = -476638    
+	return a*t1*t1 + b*t3*t3 + c*t1*t3 + d*t1 + e*t3 + f
 
 #returns new dictionaries with only the entries within the timestamp range (start_ts, end_ts)
 def keep_ts_in_range(tryDict,acqDict,relDict,start_ts,end_ts):
@@ -138,15 +147,16 @@ def get_num_div(start,end,slot_size):
 	return (end-start)/slot_size
 
 def get_bucket_proportion(start,current_start,end,acqDic,lockid,classList):
-		print "in get_bucket_proportion"
+		#print "in get_bucket_proportion, classList", classList
 		total_bucket_size = 0
 		bucket_size = 0
 		for lock in range(0, n_locks):
 			average_items = get_average_n_items_in_range(start,current_start,end,acqDic,lock,classList)
+			#print "lock:",lock, "aveage_items:",average_items
 			if lock == lockid:
 				bucket_size = average_items
 			total_bucket_size = total_bucket_size + average_items
-		print "bucket_size", bucket_size, "total_bucket_size:",total_bucket_size
+		#print "bucket_size", bucket_size, "total_bucket_size:",total_bucket_size
 		return (float(bucket_size))/total_bucket_size
 		
 def get_n_items_and_serv(start,end,nDiv,slot_size,tryDic,acqDic,relDic,namesD,n,classList,lockid):
@@ -191,17 +201,8 @@ def get_ts(tryDic,acqDic,relDic,slot_size, start, end, nDiv):
 		ts_l.append((slot_start,slot_end))
 	return ts_l
 	
-
-def get_slices(tryDic,acqDic,relDic,slot_start,slot_end):
-	print "in get_slices:"
-	(newTryDict,newAcqDict,newRelDict) = keep_ts_in_range(tryDic,acqDic,relDic,slot_start,slot_end)
-	newClassL = getClassL (newTryDict.keys(),tryDic.keys(),n) #print "newClassL:", newClassL
-	newnewClassL = filter(lambda x: x != [], newClassL)
-	classVector = tuple(map(len,newnewClassL))
-	return (newTryDict, newAcqDict,newRelDict,newnewClassL)
-
 def slotted_analyzed(start,end,nDiv,slot_size,tryDic,acqDic,relDic,namesD,n,classList):	
-	print "slotted analysis:"
+	print "slotted analysis: classList:", classList
 	throughput = []
 	s1 = [] #class 1 service time
 	s3 = [] #class 3 service time
@@ -227,7 +228,7 @@ def slotted_analyzed(start,end,nDiv,slot_size,tryDic,acqDic,relDic,namesD,n,clas
 		serv_l.append(res[1])
 		analyzed = ana[0] #ana[0] is the waiting time matrix, ana[1] is the queue length matrix
 		fill_m_and_a(measured,analyzed,m1,m3,a1,a3)
-	return serv_l, m3,a3
+	return serv_l, m3,a3, m1,a1
 		
 def get_class_end_time(start_time,classList,classID, relDic):
 	end_time = start_time
@@ -249,33 +250,42 @@ def get_throughput_from_l(l):
 	else:
 		return (0,l)
 
-def iterative_throughput_s_mva(throu1, throu3, s1, s3 ,serv,res,newClassL):
+def iterative_throughput_s_mva(throu1, throu3, s1, s3 ,serv,res,newClassL): #s1 and s3 and serv is service rate
 	lockid = 0
-	throu1 = throu1/proportions.pop(0)
-	throu3 = throu3/proportions.pop(0)
-	print "in iterative_throughput_s_mva: the real class 1 service time:", serv[0][class1_idx]
-
+	prop = proportions.pop(0)
+	i = 0
+	#print "in iterative_throughput_s_mva: the proportion:", prop
+	print "in iterative_throughput_s_mva: the real class 1 service rate:", serv[0][class1_idx]
+	print "in iterative_throughput_s_mva: the real class 1 real service time:", 1.0/serv[0][class1_idx]
+	print "in iterative_throughput_s_mva: the polyfit class 1 service time:", 1.0/s1
+	
+	#if if_print: #TOFIX: change this to convergence condition
 	for i in range(0,5): #TOFIX: change this to convergence condition
 		ana = mva_multiclass(res[0],serv,newClassL,res[3]) #do an initial analysis
 		(throu1,throu3) = get_throughput_from_l(ana[2][lockid])
-		oh = get_contention_overhead(throu1,throu3) #overhead
+		throu1_stage = throu1/prop
+		throu3_stage = throu3/prop
+		print "throughput 1:", throu1_stage, "throughput 3:",throu3_stage
+	
+		oh = get_contention_overhead(throu1_stage,throu3_stage) #overhead
+		oh_asy = get_contention_overhead_asymmetric(throu1_stage,throu3_stage) #overhead
 		#plug the service time back in
-		s1 = serv[0][class1_idx] #res_up[1] is the service time matrix
-		s1 = s1 + oh
-		serv[0][class1_idx] = s1 #res_up[1] is the service time matrix
+		s1 = 1.0/serv[0][class1_idx] #res_up[1] is the service time matrix
+		print "service 1 before plugging overhead:", s1
+		s1 = s1 + oh_asy
+		serv[0][class1_idx] = 1.0/s1 #res_up[1] is the service time matrix
+		print "overhead with the asymmetric formula:", oh_asy
 		print "service 1:", s1
-		print "throughput 1:", throu1
-		print "throughput 3:", throu3
-		#update s3 also
+		i = i + 1
 	
 	return (ana[0][1,class1_idx],ana[0][1,class3_idx]) #ana[0] is the waiting time, ana[2] is the throughput
 
-def predict_another_case(tryDic,acqDic,relDic,start_time,end_time,classL,newClassL,n_items_l,namesD,a1,b1,a3,b3,serv,if_up):
+def predict_another_case(tryDic,acqDic,relDic,start_time,end_time,classL,newClassL,n_items_l,namesD,a1,b1,a3,b3,serv,if_up): #serv has the service rates, not time
 	predicted3 = []
 	predicted1 = []
 	predicted3_with_contention = []
 	predicted1_with_contention = []
-	print "in predict_another_case,classL:", classL, " serv length:",len(serv)
+	#print "in predict_another_case,classL:", classL, " serv length:",len(serv)
 	(newTryDict,newAcqDict,newRelDict) = keep_ts_in_range(tryDic,acqDic,relDic,start_time,end_time)
 	res = multi_analyze(newTryDict, newAcqDict, newRelDict, namesD, classL)
 	if if_up:
@@ -284,21 +294,16 @@ def predict_another_case(tryDic,acqDic,relDic,start_time,end_time,classL,newClas
 		class3_idx = 0
 	j = 0
 	for n in n_items_l: #n is the number of items to be done
-		#res[1][1][class1_idx] = 1.0/450000.7 #res_up is the service time matrix
 		if j < (len(serv)):
-			#for i in range(0,len(res[1])/2):
-			#serv[j][i*2+1][class3_idx] = 1.0/(a*n+b) #i*2+1 are the indices of all the locks in the queueing network
 			serv[j][1][class3_idx] = 1.0/(a3*n+b3) #i*2+1 are the indices of all the locks in the queueing network
 			if if_up:
-					#serv[j][i*2+1][class1_idx] = 1.0/(a*n+b) #res_up[1] is the service time matrix
-				serv[j][1][class1_idx] = 1.0/(a1*n+b1) #res_up[1] is the service time matrix
+				serv[j][1][class1_idx] = 1.0/(a3*n+b3) #res_up[1] is the service time matrix
 			ana = mva_multiclass(res[0],serv[j],newClassL,res[3]) #the second parameter is the service rate, not the service time
-
-			#ana[2][0] is the throughput
 			(throu1,throu3) = get_throughput_from_l(ana[2][0])
-			print "throughput:", ana[2][0] #throughput of lock 0
+			#print "throughput before calling iterative throughput", ana[2][0] #throughput of lock 0
+			#print "number of items:", n
 			if if_up:
-				(w1,w3) = iterative_throughput_s_mva(throu1, throu3, serv[j][1][class1_idx], serv[j][1][class3_idx] ,serv[j],res,newClassL)
+				(w1,w3) = iterative_throughput_s_mva(throu1, throu3, serv[j][1][class1_idx], serv[j][1][class3_idx], serv[j], res, newClassL)
 				predicted1_with_contention.append(w1)
 				predicted3_with_contention.append(w3)
 			predicted1.append(ana[0][1,class1_idx]) #ana[0] is the waiting time, ana[2] is the throughput
@@ -321,69 +326,68 @@ def pre_process(filename,n):
 	del relDic[seq_stage_id]
 	threads_l = tryDic.keys()
 	classList = [threads_l[0:n],threads_l[n:2*n],threads_l[2*n:3*n]]
-	#print "In pre_process, classList:", classList
 
 	end_time = maxT(relDic.values(),1)[ts_idx] #the timestamp is the second element in the tuple
 	start_time = minT(tryDic.values(),1)[ts_idx]
 	start_run = start_time
 	#print "start_time:", start_time, "end_time:", end_time,"Total time:", end_time - start_time
 	class1EndT =  get_class_end_time(start_time,classList, class1_idx, relDic) #0 is the class index
-	#print "class1EndT:",class1EndT
+	class2EndT = get_class_end_time(start_time,classList,class2_idx, relDic)
+	class3EndT = get_class_end_time(start_time,classList,class3_idx, relDic)
+	print "class1EndT:", class1EndT
+	print "class2EndT:", class2EndT
+	print "class3EndT:", class3EndT
 
 	l = filter(lambda x: x!= [], relDic.values())
 	startRel = minT(l,1)[1]
 	endRel = maxT(l,1)[1]
-	return tryDic,acqDic,relDic,namesD,start_time,end_time,class1EndT,classList,classL
-
-def check_fitting(a_up,b_up,a_down,b_down,class_idx):
-	n = 10
-	nDiv = 10
-	file_target = './dedup_run_on_halvan/dedup_10th_32c_random_input/dedup.native.10th.halvan.random'
-	tryDic,acqDic,relDic,namesD,start_time,end_time, class1EndT,classList,classL = pre_process(file_target,n)
-	slot_size = get_slot_size(start_time,class1EndT,nDiv)
-
-	lockid = 0
-	serv_up_10, items_up_10 = get_n_items_and_serv(start_time,class1EndT,2,0,tryDic,acqDic,relDic,namesD,n,classList,lockid)
-	serv_down_10,items_down_10 =  get_n_items_and_serv(class1EndT,end_time,0,slot_size,tryDic,acqDic,relDic,namesD,n,classList,lockid)
-
-	#print_list_vertical(serv_up_10,"serv_up_10")
-	#print "uphill service time calculated with (a,b) of (2,2,2)"
-	print "#items","\t","Real service time:", "\t","Curve fitting service time with parameters of 222"
-	for index,i in enumerate(items_up_10):
-		print i,"\t",serv_up_10[class_idx][index],"\t",i*a_up + b_up
-	for index,i in enumerate(items_down_10):
-		print i,"\t",serv_down_10[class_idx][index],"\t",i*a_down + b_down
+	return tryDic,acqDic,relDic,namesD,start_time,end_time,(class1EndT,class2EndT,class3EndT),classList,classL
 
 def get_polyfit(data_file):
 	a_up = a_down = b_up = b_down = 0
-	n = 2
+	#n = 2
+	n = 4
 	nDiv = 10
 	class1_idx = 0 
 	class3_idx = 1
 	lockid = 0
 	print "analyzing 222"
-	tryDic,acqDic,relDic,namesD,start_time,end_time, class1EndT,classList,classL = pre_process(data_file,n)
+	tryDic,acqDic,relDic,namesD,start_time,end_time, stage_end_time,classList,classL = pre_process(data_file,n)
+
+	class1EndT = stage_end_time[class1_idx]
+	class2EndT = stage_end_time[class2_idx]
+	class3EndT = stage_end_time[class3_idx]
+
 	slot_size = get_slot_size(start_time,class1EndT,nDiv)
-	serv_up, items_up = get_n_items_and_serv(start_time,class1EndT,2,0,tryDic,acqDic,relDic,namesD,n,classList,lockid)
+
+	serv_up1, items_up1 = get_n_items_and_serv(start_time,class2EndT,2,0,tryDic,acqDic,relDic,namesD,n,classList,lockid)
+	serv_up2, items_up2 = get_n_items_and_serv(start_time,class2EndT,2,0,tryDic,acqDic,relDic,namesD,n,classList,lockid)
 
 	(a1_up,b1_up) = polyfit(items_up,serv_up[class1_idx],1)
 	(a3_up,b3_up) = polyfit(items_up,serv_up[class3_idx],1)
+
+	print_list_vertical(items_up,"number of items up")
+	print_list_vertical(serv_up[class1_idx],"service time class 1 up")
+	print_list_vertical(serv_up[class3_idx],"service time class 3 up")
+	
 	print "result of uphill polyfit: a1_up:", a1_up, "b1_up:",b1_up
 	print "result of uphill polyfit: a3_up:", a3_up, "b3_up:",b3_up
 
-	#print "length of items_down:", len(items_down), "length of serv_down:",len(serv_down)
 	serv_down,items_down =  get_n_items_and_serv(class1EndT,end_time,0,slot_size,tryDic,acqDic,relDic,namesD,n,classList,lockid)
 	(a3_down,b3_down) = polyfit(items_down,serv_down[class3_idx],1)
 	print "result of downhill polyfit:", a3_down, "b3_down:",b3_down
-	#check_fitting(a_up,b_up,a_down,b_down,class_idx)
-	#print "calculating the proportion of lock0 hashtable uphill:", items_up[lockid]/sum(items_up) 
+	print_list_vertical(items_down,"number of items down")
+	print_list_vertical(serv_down[class3_idx],"service time class 3 down")
+
 	return (a1_up,b1_up,a3_up,b3_up,a3_down,b3_down)
 
 def get_derivative_analysis():
-	if_predict = int(sys.argv[2])
-	file_base = './dedup_run_on_halvan/halvan.dedup.random.2th'
+	if_predict = int(sys.argv[1])
+	#file_base = './dedup_run_on_halvan/halvan.dedup.random.2th'
+	file_base = './dedup_run_on_halvan/halvan.dedup.random.4th'
 	file_target = './dedup_run_on_halvan/dedup_10th_32c_random_input/dedup.native.10th.halvan.random'
-	n_base = 2 #num_t_base
+	#n_base = 2 #num_t_base
+	n_base = 4
 	n_target = 10 
 	nDiv = 10
 	class_id = 3
@@ -392,10 +396,10 @@ def get_derivative_analysis():
 	tryDic,acqDic,relDic,namesD,start_time,end_time, class1EndT,classList,classL = pre_process(file_target,n_target)
 	slot_size = get_slot_size(start_time,class1EndT,nDiv)
 
-	serv_l_up,m3_up,a3_up = slotted_analyzed(start_time,class1EndT,2,0,tryDic,acqDic,relDic,namesD,n_target,classList) #devide the part before class 1 threads finish into 2 parts
-	serv_l_down,m3_down,a3_down = slotted_analyzed(class1EndT,end_time,0,slot_size,tryDic,acqDic,relDic,namesD,n_target,classList)
+	serv_l_up,m3_up,analyzed3_up,m1_up,analyzed1_up = slotted_analyzed(start_time,class1EndT,2,0,tryDic,acqDic,relDic,namesD,n_target,classList) #devide the part before class 1 threads finish into 2 parts
+	serv_l_down,m3_down,a3_down,m1_down,a1_down = slotted_analyzed(class1EndT,end_time,0,slot_size,tryDic,acqDic,relDic,namesD,n_target,classList)
 	print_list_vertical(m3_up + m3_down ,"measured3")
-	print_list_vertical(a3_up + a3_down ,"analyzed3")
+	print_list_vertical(analyzed3_up + a3_down ,"analyzed3")
 
 	#**************************************************************#
 	#start prediction, parse the base_file#
@@ -403,12 +407,12 @@ def get_derivative_analysis():
 	#get the parameters for the function (service time 3) = a * (#items in the hashtable) + b
 	#here we assume a linear relationship between the number of items and the class 3 threads (compress threads) servicet item
 	if  if_predict:
-		(a1_up,b1_up,a3_up,b3_up,a3_down,b3_down) = get_polyfit(sys.argv[1])
+		(a1_up,b1_up,a3_up,b3_up,a3_down,b3_down) = get_polyfit(file_base)
 
 		uphill_classL = (10,10,10)
 		n_items_l = [216, 654, 1068, 1495, 1981, 2499, 3063, 3667, 4243, 4728]
 		print "predicting uphill:"
-		p1_up,p3_up,p1_w_cont_up, p3_w_cont_up = predict_another_case(tryDic,acqDic,relDic,start_time,class1EndT,classL,uphill_classL,n_items_l,namesD,a1_up,b1_up,a3_up,b3_up,serv_l_up,1)
+		p1_up,p3_up,p1_w_cont_up, p3_w_cont_up = predict_another_case(tryDic,acqDic,relDic,start_time,class1EndT,classL,uphill_classL,n_items_l,namesD,a1_up,b1_up,a3_down,b3_down,serv_l_up,1)
 
 		#print_list_vertical(predicted3_up ,"predicted3_up")
 		n_items_l = [4743, 4318, 3882, 3439, 2983, 2549, 2131, 1692, 1237, 781, 275]
@@ -423,16 +427,22 @@ def get_derivative_analysis():
 	#for index,i in enumerate(n_items_l):
 		#print i,"\t",m3_up[index],a3_up[index],p3_up[index],p3_w_cont_up[index]
 	#print len(m3_up),len(a3_up), len(p3_up),len(p3_w_cond_up)
-	print "m3_up:"
+	print "measured3_up:"
 	print m3_up
-	print "a3_up:"
-	print a3_up
+	print "analyzed3_up:"
+	print analyzed3_up
 	print "p3_up:"
 	print p3_up
 	print "p3_w_cont__up:"
 	print p3_w_cont_up
+	print "measured1_up:"
+	print m1_up
+	print "analyzed1_up:"
+	print analyzed1_up
+	print "predicted1_up:"
+	print p1_up
+	print "predicted1 with contention up:"
+	print p1_w_cont_up
 	#print_list_vertical(predicted3_up + predicted3_down ,"predicted3")
 
 get_derivative_analysis()
-#analyze_measured()
-#get_polyfit(sys.argv[1])
